8d70c5fde1258cda7656abc21b444379f69f6cdd
Jackon Yang <jiekunyang@gmail.com>
2015-05-13 01:19:45 +0800

    backup provider

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 python manage.py dumpdata --indent=4 base > base/fixtures/initial_data.json
+python manage.py dumpdata --indent=4 provider > provider/fixtures/initial_data.json
 
 rm -rf init_data
 cp -R media init_data
