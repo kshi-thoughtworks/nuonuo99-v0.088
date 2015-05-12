fb82f67af574e8e98e7ebec6833d8c0a07a1b5c5
Jackon Yang <jiekunyang@gmail.com>
2015-05-13 01:17:44 +0800

    show provider avatar in admin

--- provider/adminx.py
+++ provider/adminx.py
@@ -4,7 +4,15 @@ from provider.models import ProviderInfo
 
 
 class ProviderInfoAdmin(object):
-    list_display = ("name", "contact", "phone", "address", "level", "desc")
+
+    def show_avatar(self, ins):
+        return '<img src="%s" style="width:180px;" title="%s"/>' % (ins.avatar.url, ins.name)
+
+    show_avatar.short_description = "运营商头像"
+    show_avatar.allow_tags = True
+    show_avatar.is_column = True
+
+    list_display = ("name", "show_avatar", "contact", "phone", "address", "level", "desc")
 
 
 xadmin.site.register(ProviderInfo, ProviderInfoAdmin)
