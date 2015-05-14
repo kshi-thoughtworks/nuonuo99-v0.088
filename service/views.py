bdf96c99fdb5439220e9ccaf03a5790765341016
Jackon Yang <jiekunyang@gmail.com>
2015-05-14 10:42:48 +0800

    mc filter init. gender

--- service/views.py
+++ service/views.py
@@ -3,6 +3,8 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
+from service.models import MC
+
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
@@ -13,7 +15,17 @@ def filter_makeup(request):
 
 
 def filter_mc(request):
+    # print request.GET
+
+    kwargs = dict()
+
+    # gender filter
+    gender = request.GET.get('gender', '0')
+    if gender != '0':
+        kwargs['gender'] = gender
+
     content = {
+        'data_set': MC.objects.filter(**kwargs),
             }
     return render_to_response('mc.html', RequestContext(request, content))
 
