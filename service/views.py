0444e0f01ff2c20a5c7e0ac51eef51457ad58b55
Jackon Yang <jiekunyang@gmail.com>
2015-05-14 11:23:42 +0800

    expert filter and MC

--- service/views.py
+++ service/views.py
@@ -3,27 +3,32 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
-from service.models import MC
+from service.models import MC, MakeUp
 
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
+def expert_filter(query_set):
+    kwargs = dict()
+
+    # gender filter
+    gender = query_set.get('gender', '0')
+    if gender != '0':
+        kwargs['gender'] = gender
+
+    return kwargs
+
+
 def filter_makeup(request):
+    kwargs = expert_filter(request.GET)
     content = {
+        'data_set': MakeUp.objects.filter(**kwargs),
             }
     return render_to_response('makeup.html', RequestContext(request, content))
 
 
 def filter_mc(request):
-    # print request.GET
-
-    kwargs = dict()
-
-    # gender filter
-    gender = request.GET.get('gender', '0')
-    if gender != '0':
-        kwargs['gender'] = gender
-
+    kwargs = expert_filter(request.GET)
     content = {
         'data_set': MC.objects.filter(**kwargs),
             }
