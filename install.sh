de5451e0caa76524baaa5a69fd9226e812181759
Jackon Yang <jiekunyang@gmail.com>
2015-05-17 16:15:13 +0800

    load expert data later

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 rm -f db.sqlite3
+rm expert/fixtures/initial_data.json
+
 python manage.py syncdb --noinput
 python manage.py createsuperuser --username=jackon --email=jiekunyang@gmail.com
 
 python manage.py check_permissions  # required by userena
+
+git checkout expert/fixtures/initial_data.json
+
+python manage.py loaddata expert/fixtures/initial_data.json
 
 rm -rf media
 cp -R init_data media
