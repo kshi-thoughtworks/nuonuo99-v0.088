539af1e6e5bbe33b03c816d00e6dd352c1157c23
Jackon Yang <jiekunyang@gmail.com>
2015-05-17 11:46:53 +0800

    refactor add_to_cart. use product_key

--- shop/views.py
+++ shop/views.py
@@ -12,34 +12,64 @@ import std_product.models
 
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
+type_map = {
+    'makeup': expert.models.MakeUp,
+    }
+
+def parse_product_key(product_key):
+    """return product obj
+
+    return None, if error occurs
+    raise ObjectDoesNotExist, if there is no error but obj not exist
+    """
+    params = product_key.split('_')
+    if len(params) != 2:
+        return  # format error
+
+    type_key, obj_id = params
+
+    if type_key not in type_map:
+        return  # type error
+
+    try:
+        obj = type_map[type_key].objects.get(id=obj_id)
+    except MultipleObjectsReturned:
+        return  # error
+
+    return obj
+
 
 # login required
 def add(request):
-    amount = int(request.GET.get('amount'))
+    raw_amount = request.GET.get('amount')
+    product_key = request.GET.get('product_key', '')
 
-    obj_id = 1
-    obj_model = expert.models.MakeUp
+    error_msg = ''
 
-    msg = 'success'
-    res_code = 0
-    
-    if amount and obj_model and obj_id:
-        obj = obj_model.objects.get(id=obj_id)
-        try:
-            product_type = ContentType.objects.get_for_model(obj_model)
-            item = CartInfo.objects.get(buyer=request.user, object_id=obj_id, content_type__pk=product_type.id)
-        except ObjectDoesNotExist:
-            CartInfo(buyer=request.user, content_object=obj, amount=amount).save()
-        else:
-            item.amount += amount
-            item.save()
+    try:
+        amount = int(raw_amount)
+    except ValueError:
+        error_msg += 'param error. amount is necessary and number is required.'
+
+    try:
+        product_obj = parse_product_key(product_key)
+    except ObjectDoesNotExist:
+        error_msg += 'param error. product_key error'
     else:
-        res_code = 1
-        msg = 'param error'
+        if product_obj is None:
+            error_msg += 'param error. product_key error'
 
-    response_data = {
-            'res_code': '0',  # success
-            'message': msg,
-            }
+    if error_msg:
+        return HttpResponse(json.dumps({'res_code': '1', 'message': error_msg}), content_type="application/json")
 
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    # add product_obj, amount, user to CartInfo
+    try:
+        product_type = ContentType.objects.get_for_model(product_obj)
+        item = CartInfo.objects.get(buyer=request.user, object_id=product_obj.id, content_type__pk=product_type.id)
+    except ObjectDoesNotExist:  # add new item to cart
+        CartInfo(buyer=request.user, content_object=product_obj, amount=amount).save()
+    else:
+        item.amount += amount
+        item.save()
+
+    return HttpResponse(json.dumps({'res_code': '0', 'message': 'success'}), content_type="application/json")
