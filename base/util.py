25992d13030cea941fc1ad1ae2dd94033332c34f
Jackon Yang <jiekunyang@gmail.com>
2015-05-22 23:41:03 +0800

    get filter set

--- base/util.py
+++ base/util.py
@@ -2,4 +2,18 @@
 from base.models import DiyFilter
 
 def get_choice_set(key):
-    return [(item.value, item.value_disp) for item in DiyFilter.objects.get(name=key)]
+    return [(item.value, item.value_disp) for item in DiyFilter.objects.filter(name=key)]
+
+def get_filter_sets(key):
+    kwargs = dict()
+    def add_item(item):
+        if item.name in kwargs:
+            kwargs[item.name]['values'].append((item.value, item.value_disp))
+        else:
+            kwargs[item.name] = {
+                'disp_name': item.get_name_display(),
+                'values': [(item.value, item.value_disp)],
+                }
+    for item in DiyFilter.objects.filter(scen=key).order_by('order'):
+        add_item(item)
+    return kwargs
