2e43870f628601bf7b209825246e296b014a960f
Jackon Yang <jiekunyang@gmail.com>
2015-05-27 13:14:33 +0800

    show in admin

--- expert/adminx.py
+++ expert/adminx.py
@@ -1,10 +1,10 @@
 #-*- coding:utf-8 -*-
 import xadmin
-from expert.models import MC, MakeUp
+from expert.models import MC, MakeUp, Photographer, VedioGuys
 
 
 def full_cols(*args):
-    return ("product_key", "avatar_html", "name", "price") + args + ("gender", "wed_sty", "desc")
+    return ("product_key", "avatar_html", "name", "price") + args + ("gender", "sla", "desc")
 
 
 class ExpertAdmin(object):
@@ -17,12 +17,22 @@ class ExpertAdmin(object):
 
 
 class McAdmin(ExpertAdmin):
-    list_display = full_cols('loc_native', 'lang')
+    list_display = full_cols('wed_sty', 'loc_native', 'lang')
 
 
 class MakeUpAdmin(ExpertAdmin):
-    list_display = full_cols('is_cosmetics_imported')
+    list_display = full_cols('wed_sty', 'is_cosmetics_imported')
+
+
+class PhotographerAdmin(ExpertAdmin):
+    list_display = full_cols('device_brand', 'is_full_frame', 'allow_teamwork')
+
+
+class VedioGuysAdmin(ExpertAdmin):
+    list_display = full_cols('use_camera',)
 
 
 xadmin.site.register(MC, McAdmin)
 xadmin.site.register(MakeUp, MakeUpAdmin)
+xadmin.site.register(Photographer, PhotographerAdmin)
+xadmin.site.register(VedioGuys, VedioGuysAdmin)
