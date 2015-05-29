19f74cb2f49ca50acae2c31dcb90f6959d5db01d
Jackon Yang <jiekunyang@gmail.com>
2015-05-29 13:27:32 +0800

    show diy link in home page

--- base/views.py
+++ base/views.py
@@ -2,6 +2,13 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+import base.choices as choise_set
+
 
 def home(request):
-    return render_to_response('home.html', RequestContext(request))
+    content = {
+        'flower': choise_set.C_FLOWER_CATE,
+        'av': choise_set.C_AV_CATE,
+        'stage': choise_set.C_STAGE_CATE,
+        }
+    return render_to_response('home.html', RequestContext(request, content))
