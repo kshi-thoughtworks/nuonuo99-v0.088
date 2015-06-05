c99f09c7e9a3f7de3e6f133d271f478049b2c3b0
Jackon Yang <jiekunyang@gmail.com>
2015-06-05 12:46:43 +0800

    data

--- backup.sh
+++ backup.sh
@@ -4,6 +4,7 @@ python manage.py dumpdata --indent=4 base > base/fixtures/initial_data.json
 python manage.py dumpdata --indent=4 provider > provider/fixtures/initial_data.json
 python manage.py dumpdata --indent=4 location> location/fixtures/initial_data.json
 python manage.py dumpdata --indent=4 expert > expert/fixtures/initial_data.json
+python manage.py dumpdata --indent=4 std_product > std_product/fixtures/initial_data.json
 
 rm -rf init_data
 cp -R media init_data
