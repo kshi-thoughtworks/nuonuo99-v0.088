f814cc09ca48f6d4e65a433e00a81ed50c493259
Jackon Yang <jiekunyang@gmail.com>
2015-06-06 21:16:59 +0800

    clean code

--- expert/urls.py
+++ expert/urls.py
@@ -5,8 +5,4 @@ from expert import views
 
 urlpatterns = (
     url(r'^(\w+)$', views.expert_home, name='expert_home'),
-    url(r'^mc/q$', views.McList.as_view(), name='mc_list'),
-    url(r'^makeup/q$', views.MakeUpList.as_view(), name='makeup_list'),
-    url(r'^photographer/q$', views.PhotographerList.as_view(), name='photographer_list'),
-    url(r'^vedioguys/q$', views.VedioGuysList.as_view(), name='vedioguys_list'),
 )
--- expert/views.py
+++ expert/views.py
@@ -2,22 +2,10 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-# REST framework
-from rest_framework import generics
-from rest_framework import permissions
-
-
+import base.choices as choice_set
 import expert.models
 import expert.filters
 import expert.serializers
-
-from expert.models import mc, makeup, photographer, vedioguys
-
-
-from expert.filters import mc_filter, makeup_filter, photographer_filter, vedioguys_filter
-from expert.serializers import mc_serializer, makeup_serializer, photographer_serializer, vedioguys_serializer
-
-import base.choices as choice_set
 
 
 def expert_home(request, type='mc'):
@@ -34,27 +22,3 @@ def expert_home(request, type='mc'):
         'disp_name': model._meta.verbose_name
         }
     return render_to_response('expert.html', RequestContext(request, content))
-
-
-class McList(generics.ListCreateAPIView):
-    queryset = mc.objects.all()
-    serializer_class = mc_serializer
-    filter_class = mc_filter
-
-
-class MakeUpList(generics.ListCreateAPIView):
-    queryset = makeup.objects.all()
-    serializer_class = makeup_serializer
-    filter_class = makeup_filter
-
-
-class PhotographerList(generics.ListCreateAPIView):
-    queryset = photographer.objects.all()
-    serializer_class = photographer_serializer
-    filter_class = photographer_filter
-
-
-class VedioGuysList(generics.ListCreateAPIView):
-    queryset = vedioguys.objects.all()
-    serializer_class = vedioguys_serializer
-    filter_class = vedioguys_filter
